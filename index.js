a2d500dd689cdcd90ca50771dbd2734b82edb902
sukanta255 <sukantap872@gmail.com>
2024-05-20 22:46:20 +0530

    some bug fix

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose")
-const dotenv = require("dotenv");
+// const connectDB = require('./config/db');
+const cors = require("cors");
+require('dotenv').config();
 const eventRoute = require("./routes/eventRoute")
 
 
-dotenv.config();
-const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose")
+
+
 
 
  
@@ -14,13 +16,15 @@ mongoose.connect(process.env.MONGO_URL)
 .then(_=> console.log("db connection successful"))
 .catch(err=> console.log("db connection fail"))
 
+// connectDB();
 
 
 
-app.use(express.json({limit: '50mb'}));
+
 app.use(cors()) 
+app.use(express.json());
 app.use("/api/events", eventRoute)
 
 
 
-app.listen(process.env.PORT||5000, _=> console.log("backend server is running on port: "+ process.env.PORT))
+app.listen(process.env.PORT||5000, ()=> console.log("backend server is running on port: "+ process.env.PORT))
